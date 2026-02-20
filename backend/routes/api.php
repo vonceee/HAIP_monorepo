a35fac40effaf74caf77430f6cd88096a43a3a37
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-02-21 07:23:58 +0800

    cc

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
 });
 
