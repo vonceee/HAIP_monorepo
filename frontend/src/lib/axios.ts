adf008ac7c184aa48c576a442f2927098cc3ce98
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-02-21 06:52:38 +0800

    aa

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -8,4 +8,18 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// Add CSRF token interceptor for Sanctum
+api.interceptors.request.use((config) => {
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("XSRF-TOKEN="))
+    ?.split("=")[1];
+
+  if (token) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+
+  return config;
+});
+
 export default api;
